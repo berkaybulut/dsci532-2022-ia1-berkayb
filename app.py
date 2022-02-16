7d3cb1b2efde30708eba16bb35d2b4d4da08acc1
Berkay Bulut <mberkaybulut@gmail.com>
2022-02-15 21:00:44 -0500

    Update chart

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-from dash import Dash, dcc, html, Input, Output
-from vega_datasets import data
 import os
+from dash import Dash, dcc, html, Input, Output
+import altair as alt
+from vega_datasets import data
 
+    
+plot_data = data.cars()
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def plot_altair(xmax):
+    chart = alt.Chart(plot_data[plot_data['Year'] < xmax]).mark_line().encode(
+        x='Year',
+        y='Deaths')
+    return chart.to_html()
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
-])
-
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+        html.Iframe(
+            id='scatter',
+            srcDoc=plot_altair(xmax=2017),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        dcc.Slider(id='xslider', min=1900, max=2017)])
+        
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xslider', 'value'))
+def update_output(xmax):
+    return plot_altair(xmax)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
