b5be074234f5a2b171b4fd6658c8cb54027964d3
Berkay Bulut <mberkaybulut@gmail.com>
2022-02-15 22:15:51 -0500

    Update app-code

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
-import os
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, html, dcc, Input, Output
 import altair as alt
 from vega_datasets import data
 
-    
-plot_data = data.cars()
 
-def plot_altair(xmax):
-    chart = alt.Chart(plot_data[plot_data['Year'] < xmax]).mark_line().encode(
-        x='Year',
+plot_data = data.disasters()
+
+def plot_altair(xmin,xmax):
+    chart = alt.Chart(plot_data[plot_data['Year'].between(xmin,xmax)]).mark_line().encode(
+        x=alt.X('Year'),
         y='Deaths')
     return chart.to_html()
 
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
+server = app.server
+
 app.layout = html.Div([
         html.Iframe(
-            id='scatter',
-            srcDoc=plot_altair(xmax=2017),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Slider(id='xslider', min=1900, max=2017)])
+            id='line',
+            srcDoc=plot_altair(xmin=1901, xmax=2017),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'})
+        ])
         
 @app.callback(
-    Output('scatter', 'srcDoc'),
-    Input('xslider', 'value'))
-def update_output(xmax):
-    return plot_altair(xmax)
+    Output('line', 'srcDoc'),
+    )
+def update_output(xmin,xmax):
+    return plot_altair(xmin,xmax)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
